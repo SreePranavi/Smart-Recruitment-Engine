25d93bca157b8de919cc35024f238647fde8789e
SreePranavi <letters.to.pranavi@gmail.com>
2023-12-04 18:33:53 +0530

    transcript and summary

--- smart-recruit.py
+++ smart-recruit.py
@@ -7,13 +7,29 @@ from datetime import timedelta
 import pandas as pd
 
 from cosine_similarity import CosineSimilarityEngine
+from speech2text import Speech2TextEngine,TextSummarizationEngine
 
 
-@st.cache_data(ttl=timedelta(days=1))
-def setup_engine():
+@st.cache_resource(ttl=timedelta(days=1))
+def setup_cossim_engine():
     cosine_similarity_engine = CosineSimilarityEngine()
     cosine_similarity_engine.train_model()
     return cosine_similarity_engine
+
+@st.cache_resource(ttl=timedelta(days=1))
+def setup_conversion_engine():
+    conversion_engine = Speech2TextEngine()
+    return conversion_engine
+
+@st.cache_resource(ttl=timedelta(days=1))
+def setup_summarization_engine():
+    summarization_engine = TextSummarizationEngine()
+    summarization_engine.load_model()
+    return summarization_engine
+@st.cache_resource(ttl=timedelta(days=1))
+def transcript_summary():
+    transcript = conversion_engine.conversion('harvard.wav')
+    summarization_engine.summarize(transcript)
 
 st.set_page_config(
     page_title="Smart Recruitment Engine",
@@ -24,9 +40,11 @@ st.title("""Smart Recruitment Engine""")
 
 with st.expander("Help"):
     st.write("""Help meeeeeee""")
-    st.write("LOREM IPSUM")
 
-engine = setup_engine()
+cossim_engine = setup_cossim_engine()
+conversion_engine = setup_conversion_engine()
+summarization_engine = setup_summarization_engine()
+
 submitted = False
 
 with st.form("SRE Form"):
@@ -39,17 +57,19 @@ with st.form("SRE Form"):
 
     if wav_audio_data is not None:
         st.audio(wav_audio_data, format='audio/wav')
+        
+        transcript_summary()
 
     submitted = st.form_submit_button("Submit")
 
 
 if submitted :
     
-    roles_report = engine.result(['Kubernetes'])
+    roles_report = cossim_engine.result(['Kubernetes'])
 
     with st.container():
         st.header("Suitable Roles")
-        st.subheader("The suggested roles for the candidate with ID "+ str(input_candidate_id)+" in descending order of match score are: ")
-        roles_report_pd = pd.DataFrame(data=roles_report, columns=['Roles','Similarity Score'])
-        st.dataframe(roles_report, use_container_width = True)
+        st.subheader("The suggested roles for the candidate with ID "+ str(input_candidate_id)+" in descending order of match score are : ")
+        roles_report_pd = pd.DataFrame(data=roles_report, columns=('Roles','Similarity Score'))
+        st.dataframe(roles_report, use_container_width = True, column_config={"":"Role","value":"Score"})
         #st.write(roles_report)
--- speech2text.py
+++ speech2text.py
@@ -1,18 +1,16 @@
 import speech_recognition as sr
 from transformers import BartForConditionalGeneration, BartTokenizer
 
-
 class Speech2TextEngine:
 
-    def __init__(self, audio_file):
+    def __init__(self):
         self.recognizer = sr.Recognizer()
-        self.audio_file = audio_file
 
-    def conversion(self):
+    def conversion(self, audio_file):
         
         # Reading Audio file as source - listening to the audio file and store in audio_text variable
 
-        with sr.AudioFile(self.audio_file) as source:
+        with sr.AudioFile(audio_file) as source:
             audio_text = self.recognizer.listen(source)
 
         try:
@@ -26,8 +24,7 @@ class Speech2TextEngine:
 
 class TextSummarizationEngine:
 
-    def __init__(self, transcript):
-        self.transcript = transcript
+    def __init__(self):
         self.model_name="bart-large-cnn"
 
     def load_model(self):
@@ -37,8 +34,8 @@ class TextSummarizationEngine:
         self.tokenizer = BartTokenizer.from_pretrained(self.model_name)
         self.model = BartForConditionalGeneration.from_pretrained(self.model_name, forced_bos_token_id=0)
 
-    def summarize(self):
-        input_text = self.transcript
+    def summarize(self, transcript):
+        input_text = transcript
 
         # Tokenize and Generate Questions
         batch = self.tokenizer(input_text, return_tensors="pt")
@@ -47,11 +44,3 @@ class TextSummarizationEngine:
         # Decode and Print Questions
         summary = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
         print("Generated Summary:", summary)
-
-
-conversion_engine= Speech2TextEngine('harvard.wav')
-transcript = conversion_engine.conversion()
-
-summarization_engine = TextSummarizationEngine(transcript)
-summarization_engine.load_model()
-summarization_engine.summarize()
